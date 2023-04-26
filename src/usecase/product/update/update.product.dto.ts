4ad2ae6a8604066abc6dc9c7d3ed8397ab04060b
Rogerio Aguiar <rfaguiar1@gmail.com>
2023-04-25 22:02:42 -0300

    update product use case

---
+++ src/usecase/product/update/update.product.dto.ts
@@ -0,0 +1,11 @@
+interface InputUpdateProductDto {
+    id: string;
+    name: string;
+    price: number;
+}
+
+interface OutputUpdateProductDto {
+    id: string;
+    name: string;
+    price: number;
+}
