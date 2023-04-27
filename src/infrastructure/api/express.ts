54eb486003bca30699c740f5203af4bf60ef68fe
Rogerio Aguiar <rfaguiar1@gmail.com>
2023-04-26 22:05:27 -0300

    find product integration test

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
 import { customerRoute } from "./routes/customer.route";
+import ProductModel from "../product/repository/sequelize/product.model";
 
 export const app: Express = express();
 app.use(express.json());
@@ -15,7 +16,7 @@ async function setupDb() {
     storage: ":memory:",
     logging: false,
   });
-  await sequelize.addModels([CustomerModel]);
+  await sequelize.addModels([CustomerModel, ProductModel]);
   await sequelize.sync();
 }
 setupDb();
